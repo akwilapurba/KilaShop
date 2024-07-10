05d48299137b5eba75b73f8c12e47e5653a66115
Irfan Saf <irfansaf7@gmail.com>
2024-07-10 07:53:09 +0700

    feat: authentication

--- app.py
+++ app.py
@@ -1,16 +1,42 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask_bcrypt import Bcrypt
 from flask_wtf import FlaskForm
 from wtforms import StringField, DecimalField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.fields import PasswordField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 from pymongo import MongoClient
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
-app.secret_key = 'your_secret_key'  # Replace with your secret key
+app.secret_key = 'your_secret_key'
 
 # MongoDB connection
 client = MongoClient('mongodb://localhost:27017/')
 db = client['kilashop']
-collection = db['product']
+product_collection = db['product']
+user_collection = db['user']
+
+# Bcrypt
+bcrypt = Bcrypt(app)
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    middle_name = StringField('Middle Name')
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    phone_number = StringField('Phone Number', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+
+# Define the User Login Form using WTForms
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
 
 
 # Define the Product Form using WTForms
@@ -24,6 +50,48 @@ class ProductForm(FlaskForm):
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user_data = {
+            'username': form.username.data,
+            'email': form.email.data,
+            'password': hashed_password,
+            'first_name': form.first_name.data,
+            'middle_name': form.middle_name.data,
+            'last_name': form.last_name.data,
+            'phone_number': form.phone_number.data,
+            'address': form.address.data,
+        }
+        user_collection.insert_one(user_data)
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = user_collection.find_one({'username': form.username.data})
+        if user and bcrypt.check_password_hash(user['password'], form.password.data):
+            session['username'] = user['username']
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    flash('You have been logged out!', 'success')
+    return redirect(url_for('home'))
 
 
 @app.route('/products')
@@ -56,7 +124,7 @@ def add_product():
             'price': price,
             'image_path': image_path,
         }
-        collection.insert_one(product_data)
+        product_collection.insert_one(product_data)
 
         return redirect(url_for('index'))
     return render_template('add_product.html', form=form)
